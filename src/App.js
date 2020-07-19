265520592660fece76fbc08a56a8990b49bc00b4
latifahiriyani <latifahiriyani98@gmail.com>
2020-07-19 09:04:36 +0700

    HomePage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-App.js
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 //import Parent from './Component/Class/Parent';
 //import BootstrapComp from './Component/Class/BootstrapComp';
 import NavbarComp from './Component/Fungsional/NavbarComp';
+import HomePage from './Component/Fungsional/HomePage';
 
 //import DetailComp from './Component/Fungsional/DetailComp';
-import ListComp from './Component/Class/ListComp';
+
 
 
 //import logo from './logo.svg';
@@ -19,8 +19,8 @@ const App = () => {
     <BrowserRouter>
       <NavbarComp />
       <switch>
-
-        <Route exact path="/obat" component={ListComp} />
+      <Route exact path="/" component={HomePage} />
+        
 
 
 
---
+++ src/Component/Fungsional/HomePage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Button, Card, CardBody, CardTittle, CardText, CardSubtitle, CardImg } from 'reactstrap';
+
+
+const HomePage = (props) => {
+
+    return (
+        <div>
+            <Card>
+                <CardImg top height="300" width="100%" src="/images/1024-500.png" alt="" />
+                
+            </Card>
+            
+            <h2></h2>
+        </div>
+    )
+}
+
+export default HomePage
